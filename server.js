420ee946f0d5ef2cc99eb9be10f6fae175b85307
laurenthlee <161715110+laurenthlee@users.noreply.github.com>
2024-10-26 14:39:21 +0100

    Update port server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
     const bodyParser = require('body-parser');
 
     const app = express();
-    const PORT = 3000;
+    const PORT = process.env.PORT || 3000; 
 
     // Encode special characters in the password
     const username = 'admin';
